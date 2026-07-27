84cb785ce8db997ae033b489b425e7e3b402df4d
masthan008 <masthanvallibaba009@gmail.com>
2026-07-27 19:41:01 +0530

    Fix 404 route redirects for /admin and /creator and add suppressHydrationWarning to root layout

--- wallvault-web/src/app/layout.tsx
+++ wallvault-web/src/app/layout.tsx
@@ -32,8 +32,9 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${inter.variable} ${jetbrainsMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col font-sans">
+      <body className="min-h-full flex flex-col font-sans" suppressHydrationWarning>
         <AuthProvider>
           {children}
         </AuthProvider>
